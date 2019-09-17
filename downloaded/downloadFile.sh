ceaa3a161b03991662ba517f4cffe2546b0ce01c
chaeyoungg <chaeyoungg77@gmail.com>
2019-09-18 00:56:23 +0900

    unnecessary file delete

--- downloaded/downloadFile.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-fileName=("$@")
-
-curl http://ec2-13-209-5-1.ap-northeast-2.compute.amazonaws.com/file/image/download/${fileName}/ --output test.tar
--- transmit/UploadWeight.bat
+++ transmit/UploadWeight.bat
@@ -2,7 +2,6 @@
 
 rem 서버에 올릴 파일 title을 실행시 받는다
 set fileTitle=%1
-echo %fileTitle%
 
 rem curl 명령어로 /send에 get 요청을 보내고 받은 헤더를 cookie.txt로 저장한다. 또한 Set-Cookie를 추가적으로 추출한다
 curl -i -c cookie.txt http://ec2-13-209-5-1.ap-northeast-2.compute.amazonaws.com/file/weight/send/
